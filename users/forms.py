043c5329f0b7c13a9e8739042d5a98f86c50ed63
Tishkin Valentin <tishkin.valentin@gmail.com>
2023-11-07 22:41:29 +0300

    shrthsrth

---
+++ users/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from users.models import User
+
+
+class userLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'введите логин'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(userLoginForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class userRegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите фамилию'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя пользователя'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Введите адрес эл. почты'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Подтвердите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super(userRegisterForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class userProfileForm(UserChangeForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'readonly': True}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'readonly': True}))
+    image = forms.CharField(widget=forms.FileInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'image')
+    
+    def __init__(self,*args, **kwargs):
+        super(userProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] ='custom-file-input'
--- users/views.py
+++ users/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
+from users.forms import userLoginForm, userRegisterForm, userProfileForm
 
 
 def login(request):
-    return render(request)
+    if request.method == 'POST':
+        form = userLoginForm(data=request.POST)
+        if form.is_valid():
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = userLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context=context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = userRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return  HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = userRegisterForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context=context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = userProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = userProfileForm(instance=request.user)
+    context = {'form': form}
+    return render(request, 'users/profile.html', context=context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
